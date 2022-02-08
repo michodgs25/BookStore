bb144873ec0cd5185c82bff2fcc58cfc7b128980
Michael Hodgson <michael.hodgson@springboardit.co.uk>
2022-02-08 09:56:57 +0000

    Add project files.

---
+++ eccomerce/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using eccomerce.Models;
+
+namespace eccomerce.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<eccomerce.Models.Books> Books { get; set; }
+    }
+}
---
+++ eccomerce/Models/Books.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace eccomerce.Models
+{
+    public class Books
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        //ange(1, 100, ErrorMessage = "Name must be between 1 and 100 only!!")]
+        public string Name { get; set; }
+        [Required]
+        //Range(5, 500, ErrorMessage ="Description must be between 5 and 500 only!!")]
+        public string Description { get; set; }
+        [Required]
+        public string Author { get; set; } 
+        [Required]
+        public string Genre { get; set; }
+        [AllowHtml]
+        public string Contents { get; set; }
+        [Required]
+        public byte[] Image { get; set; }
+        public DateTime CreatedDateTime { get; set; } = DateTime.Now;
+    }
+}
